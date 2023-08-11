b09bb4767ed4a1c495af47a51af456fccad704a6
iPonzu <joao_ignaciocastro1603@hotmail.com>
2023-08-11 03:14:50 -0300

    updating UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,22 +10,29 @@ export default function Page(){
   return(
     <main>
       <nav id="navbar">
-        <header className="w-screen px-4 py-5 flex justify-content align-center">
-          <ul>
-            <a className="font-sans absolute top-0 right-10 h-8 w-20 border-2 rounded-md hover:transition-all hover:bg-cyan-950 " href="https://www.google.com" target="_blank">LinkedIn</a>
-            <a href=""></a>
-          </ul>
+        <header className="w-screen px-4 py-5 flex items-center">
+          <div className="flex align-text-top">
+              <ul>
+                <a className="font-sans absolute inset-y-4 right-48 h-8 w-16 border-2 rounded-md hover:transition-all hover:bg-cyan-950 " href="https://www.linkedin.com/in/joao-ignacio-castro-franco-634873232/" target="_blank">LinkedIn</a>
+                <a href=""></a>
+              </ul>
+          </div>
+              <div className="flex align-text-top">
+                <div className="font-sans absolute right-24 inset-y-4 h-8 w-16 border-2 rounded-md hover:transition-all hover:bg-red-950">
+                      <Link className="font-sans 12px" href="/skills">My Skills</Link>
+                  </div>
+              </div>
+                <div className="flex align-text-top">
+                  <div className="font-sans absolute inset-y-4 h-8 w-30 left-3/4 border-2 rounded-md hover:transition-all hover:bg-green-900">
+                      <Link className="font-sans 12px" href="/projects">My Projects</Link>
+                  </div>
+                </div>
         </header>
           <div className="flex items-center justify-center h-screen">
             <h1 className="font-sans">Hi! I'm João Franco, welcome to my first website <br />
               This website was made with Next.js and TailwindCSS, to show about me and my projects and skills. <br />
             </h1>
           </div>
-            <div className="flex flex-col items-center justify-center h-8">
-              <div className="text-center">
-                <Link className="" href="/skills">My Skills</Link>
-              </div>
-            </div>
       </nav>
     </main>
   )
